e899548014f3f2107aae822dccb068986e970318
Raeme Khatib <raeme.khatib@telus.com>
2022-12-12 18:24:54 -0700

    made 6 queries grouped by multiple

---
+++ 3_queries_group-by/1_assignments_per_day.sql
@@ -0,0 +1,8 @@
+SELECT day, count(*) as total_assignments 
+FROM assignments
+GROUP BY day
+ORDER BY day;
+
+
+-- Get the total number of assignments for each day of bootcamp.
+
---
+++ 3_queries_group-by/2_busy_days.sql
@@ -0,0 +1,9 @@
+SELECT day, count(*) as total_assignments 
+FROM assignments
+GROUP BY day
+HAVING count(*) >= 10
+ORDER BY day;
+
+
+
+-- Get the total number of assignments for each day of bootcamp.but only return rows where total assignments is greater than or equal to 10
